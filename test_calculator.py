41ab5eb2e3af67562be047d85b4a0609fd28bfe4
Jainandansl <nandan56562@gmail.com>
2026-03-03 15:49:42 +0000

    Initial commit

---
+++ test_calculator.py
@@ -0,0 +1,13 @@
+import unittest
+from calculator import add, subtract
+
+class TestCalculator(unittest.TestCase):
+
+    def test_add(self):
+        self.assertEqual(add(5, 3), 8)
+
+    def test_subtract(self):
+        self.assertEqual(subtract(10, 4), 6)
+
+if __name__ == '__main__':
+    unittest.main()
